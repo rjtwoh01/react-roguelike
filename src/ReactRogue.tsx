dfbdeee53798f5435c1f59b1913842d3e9962394
Ryan Twohey <rjtwoh01@gmail.com>
2021-05-15 16:47:23 -0400

    added debugging comment

--- src/ReactRogue.tsx
+++ src/ReactRogue.tsx
@@ -7,6 +7,7 @@ export interface ReactRogueInterface {
 }
 
 const ReactRogue: React.FC<ReactRogueInterface> = (props: ReactRogueInterface) => (
+    //canvas isn't currently appearing on the screen after change to props
     <canvas width={props.width * props.tileSize} height={props.height * props.tileSize} style={{ border: '1px solid black' }}/>
 )
 
