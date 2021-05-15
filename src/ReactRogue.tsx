2faecc791cbb886047222d8f617d2167138c979c
Ryan Twohey <rjtwoh01@gmail.com>
2021-05-15 18:05:31 -0400

    Fixed canvas not appearing, updated ReactRogue to use variables

--- src/ReactRogue.tsx
+++ src/ReactRogue.tsx
@@ -6,9 +6,13 @@ export interface ReactRogueInterface {
     tileSize: number;
 }
 
-const ReactRogue: React.FC<ReactRogueInterface> = (props: ReactRogueInterface) => (
-    //canvas isn't currently appearing on the screen after change to props
-    <canvas width={props.width * props.tileSize} height={props.height * props.tileSize} style={{ border: '1px solid black' }}/>
-)
+const ReactRogue: React.FC<ReactRogueInterface> = (props: ReactRogueInterface) => {
+    const canvasWidth = props.width * props.tileSize;
+    const canvasHeight = props.height * props.tileSize;
+    
+    return (
+        <canvas width={canvasWidth} height={canvasHeight} style={{ border: '1px solid black' }}/>
+    );
+}
 
 export default ReactRogue;
