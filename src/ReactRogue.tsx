294c57f1ed44dda807b18793183f35a7ac1bd666
Ryan Twohey <rjtwoh01@gmail.com>
2021-05-15 21:53:28 -0400

    Got black square to appear on canvas

--- src/ReactRogue.tsx
+++ src/ReactRogue.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 export interface ReactRogueInterface {
     width: number;
@@ -7,11 +7,27 @@ export interface ReactRogueInterface {
 }
 
 const ReactRogue: React.FC<ReactRogueInterface> = (props: ReactRogueInterface) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
     const canvasWidth = props.width * props.tileSize;
     const canvasHeight = props.height * props.tileSize;
+
+    useEffect(() => {
+        console.log('Draw to canvas')
+        const ctx = null != canvasRef.current ? canvasRef.current.getContext('2d') : null;
+        if (ctx !== null) {
+            ctx.clearRect(0,0, canvasWidth, canvasHeight);
+            ctx.fillStyle = "#000"
+            ctx.fillRect(12, 22, 16, 16);
+        }
+    })
     
     return (
-        <canvas width={canvasWidth} height={canvasHeight} style={{ border: '1px solid black' }}/>
+        <canvas
+            ref={canvasRef}
+            width={canvasWidth}
+            height={canvasHeight}
+            style={{ border: '1px solid black' }}
+        />
     );
 }
 
